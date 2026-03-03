ddd8a45116ccdd6ddbd963542410741c9f98aed3
IslamHany-dev <islamhany.cv@gmail.com>
2026-03-03 23:47:13 +0200

    change cleanup-pending users to kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        // تنظيف الحسابات غير المفعلة كل ساعة
+         $schedule->command('users:cleanup-pending')->hourly();
     }
 
     /**
